1b18f930fdf98e1258e104c0fd850691a0bd1dd3
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-09-06 05:16:28 +0100

    edit: alter package and name struct

--- entities/africa.go
+++ entities/africa.go
@@ -1,11 +1,12 @@
-package main
+package entities
 
-type Country struct {
-	Flag        string  `json:"flag"`
-	Location    string  `json:"location"`
-	Name        string  `json:"name"`
-	Capital     string  `json:"capital"`
-	Population  float64 `json:"population"`
-	Area        float64 `json:"area"`
-	Callingcode int32   `json:"callingcode"`
+type Africa struct {
+	Flag        string   `json:"flag"`
+	Location    string   `json:"location"`
+	Keywords    []string `json:"keywords"`
+	Name        string   `json:"name"`
+	Capital     string   `json:"capital"`
+	Population  float64  `json:"population"`
+	Area        float64  `json:"area"`
+	Callingcode string   `json:"callingcode"`
 }
