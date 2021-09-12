dadd65cb0b876f4d5e20c6bd5348bf058cb303c0
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-09-12 20:40:19 +0100

    edit: modify vars port

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	servicesintegration "github.com/Paulo-Lopes-Estevao/apiworld/services/servicesIntegration"
 	"github.com/gorilla/mux"
@@ -12,13 +13,16 @@ import (
 var Version string = "/v1"
 
 func main() {
+
+	port := os.Getenv("PORT")
+
 	route := mux.NewRouter()
 
 	route.HandleFunc(Version+"/africa", servicesintegration.ListAllCountryAfrica)
 	route.HandleFunc(Version+"/africa/{name}", servicesintegration.FindCountryAfrica)
 
 	fmt.Println("Server started at port 2000")
-	err := http.ListenAndServe(":2000", route)
+	err := http.ListenAndServe(":"+port, route)
 
 	if err != nil {
 		log.Println("Not Running Server...", err.Error())
