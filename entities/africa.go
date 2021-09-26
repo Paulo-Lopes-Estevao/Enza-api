41728c6bbaa9d060d76b7ddcd73707a55d18f6bb
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-09-26 23:47:18 +0100

    edit: update add entitie "base"

--- entities/africa.go
+++ entities/africa.go
@@ -1,14 +1,5 @@
 package entities
 
 type Africa struct {
-	Flag        string   `json:"flag"`
-	Location    string   `json:"location"`
-	Keywords    []string `json:"keywords"`
-	Name        string   `json:"name"`
-	Capital     string   `json:"capital"`
-	Currency    string   `json:"currency"`
-	Language    string   `json:"language"`
-	Population  float64  `json:"population"`
-	Area        float64  `json:"area"`
-	Callingcode string   `json:"callingcode"`
+	Base *Base
 }
